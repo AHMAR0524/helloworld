6c0bedcd23f260ee52179b5ef7943b941bbbe04e
AHMAR0524 <ahmar24@gmail.com>
2024-07-17 01:47:40 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -2,6 +2,7 @@ class Simple
 {
     public static void main(String []args)
     {
-        System.out.println("My First Java Program.");
+        System.out.println("My First Java Program in Batch 35");
+        System.out.println("In my DevOps class");
     }
 };
